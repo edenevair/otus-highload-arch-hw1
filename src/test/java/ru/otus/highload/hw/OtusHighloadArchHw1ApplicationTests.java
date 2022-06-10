f3b476cf359a97692a6bc1f0f8b113e09114f138
edenevair <edenevair@yandex.ru>
2022-06-10 15:30:59 +0300

    init project

---
+++ src/test/java/ru/otus/highload/hw/OtusHighloadArchHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.highload.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusHighloadArchHw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
